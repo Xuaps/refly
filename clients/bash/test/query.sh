2644825a665d8cc87fcc335bc2506ad23c34db4a
David Vilchez & Pepe Doval <dvilchezm@gmail.com & pepellou@gmail.com>
2014-06-27 09:46:56 +0000

    Use curl instead of wget
    Easier to pipe to other commands

--- clients/bash/test/query.sh
+++ clients/bash/test/query.sh
@@ -2,35 +2,35 @@
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function test_all_args {
-    slash=$(mock $scriptDir/../slash "wget")
+    slash=$(mock $scriptDir/../slash "curl")
 
 	$slash search:function,class,type:slash,java
 
-    assertCalled $slash "wget"
-    assertCalledExactly 1 $slash "wget"
-    assertCalledWith $slash "wget" "http://localhost:3000/search?reference=search&types=function&types=class&types=type&docsets=slash&docsets=java"
+    assertCalled $slash "curl"
+    assertCalledExactly 1 $slash "curl"
+    assertCalledWith $slash "curl" "http://localhost:3000/search?reference=search&types=function&types=class&types=type&docsets=slash&docsets=java"
 }
 
 function test_one_of_each {
-    slash=$(mock $scriptDir/../slash "wget")
+    slash=$(mock $scriptDir/../slash "curl")
 
 	$slash search:function:slash
 
-    assertCalledWith $slash "wget" "http://localhost:3000/search?reference=search&types=function&docsets=slash"
+    assertCalledWith $slash "curl" "http://localhost:3000/search?reference=search&types=function&docsets=slash"
 }
 
 function test_no_docset {
-    slash=$(mock $scriptDir/../slash "wget")
+    slash=$(mock $scriptDir/../slash "curl")
 
 	$slash search:function
 
-    assertCalledWith $slash "wget" "http://localhost:3000/search?reference=search&types=function"
+    assertCalledWith $slash "curl" "http://localhost:3000/search?reference=search&types=function"
 }
 
 function test_no_docset_nor_type {
-    slash=$(mock $scriptDir/../slash "wget")
+    slash=$(mock $scriptDir/../slash "curl")
 
 	$slash search
 
-    assertCalledWith $slash "wget" "http://localhost:3000/search?reference=search"
+    assertCalledWith $slash "curl" "http://localhost:3000/search?reference=search"
 }
