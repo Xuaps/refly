cbfec6a11c5abd53570c48a03aa27a83c4c27f87
Pepe Doval <pepellou@gmail.com>
2014-07-25 19:54:06 +0200

    Meaningful logs in start_worker script
    Those logs printing out "[Object]" in the screen drove me crazy :-)

--- jobs/scripts/start_worker.js
+++ jobs/scripts/start_worker.js
@@ -1,26 +1,30 @@
 var jobs=require('../');
 var config = require('config');
 // setup a worker
-var worker = require('coffee-resque').connect(config.redisConfig).worker('*', jobs)
+var worker = require('coffee-resque').connect(config.redisConfig).worker('*', jobs);
 
 // some global event listeners
 //
 // Triggered every time the Worker polls.
-worker.on('poll', function(worker, queue) {})
+worker.on('poll', function(worker, queue) {});
 
 // Triggered before a Job is attempted.
 worker.on('job', function(worker, queue, job) {
-	console.log('Starting job.');
-})
+	console.log('Starting job:');
+	console.log(job);
+});
 
 // Triggered every time a Job errors.
 worker.on('error', function(err, worker, queue, job) {
 	console.log(err);
-})
+});
 
 // Triggered on every successful Job run.
 worker.on('success', function(worker, queue, job, result) {
-	console.log('Success job:' + job + ' result:' + result);
-})
+	console.log('Success job:');
+	console.log(job);
+	console.log('Job result:');
+	console.log(result);
+});
 
-worker.start()
+worker.start();
