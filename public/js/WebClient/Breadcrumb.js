1b492d5a2f0beb86bea6e95f0d2c3704e8c8c667
Pepe Doval <pepellou@gmail.com>
2014-07-27 03:41:39 +0200

    Avoid %20 in Breadcrumb items shown

--- public/js/WebClient/Breadcrumb.js
+++ public/js/WebClient/Breadcrumb.js
@@ -9,7 +9,7 @@ var Breadcrumb = {
     show: function(data) {
         var items = [];
         data.forEach(function(item) {
-            items.push('<a href="' + item.uri + '"' + (item.current ? ' class="current"' : '') + '>' + item.text + '</a>');
+            items.push('<a href="' + item.uri + '"' + (item.current ? ' class="current"' : '') + '>' + item.text.replace(/%20/g, ' ') + '</a>');
         });
         $(BREADCRUMB).html(items.join(' &gt; '));
         $(BREADCRUMB).css('display', 'block');
