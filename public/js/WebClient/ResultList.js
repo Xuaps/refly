128227a42621dea60cf77fde19b4375988ef2937
Pepe Doval <pepellou@gmail.com>
2014-07-31 09:20:51 +0200

    JADE template for search results

--- public/js/WebClient/ResultList.js
+++ public/js/WebClient/ResultList.js
@@ -1,34 +1,11 @@
-var RESULTS = '#results';
-
-var Color = {
-
-    forType: function(type) {
-        var colors = {
-            C: '#0f0',
-            F: '#f00',
-            P: '#88f'
-        };
-        return colors[type] || 'white';
-    }
-
-};
-
 var ResultList = {
 
     reset: function() {
-        $(RESULTS).html('');
+        $('#results').html('');
     },
 
     show: function(results) {
-        $(RESULTS).html('');
-        results.forEach(function(result) {
-            var type = result.type.substring(0, 1).toUpperCase();
-            $(RESULTS).append(
-                '<tr><td class="type" style="background-color: ' + Color.forType(type) + '">' + type +
-                '</td><td class="separator">' +
-                '</td><td><a href="' + result.uri + '">' + result.reference + '</a> <i>' + result.docset + '</i></td></tr>'
-            );
-        });
-    },
+        jade.render($('#results')[0], 'search-results', { results: results });
+    }
 
 };
