ab80b73c916f51e993851ab547d0926ee219f2ee
David Vilchez <dvilchez@xuaps.com>
2014-08-12 12:14:28 +0200

    added progress bar to spider, pending refactor to decouple

--- app/slash_spider.js
+++ app/slash_spider.js
@@ -2,18 +2,25 @@ var q = require('q');
 var cheerio = require('cheerio');
 var request = require('./slash_request');
 var Map = require('hashmap').HashMap;
+var ProgressBar = require('progress');
 
 exports.run = function(domain, path, selector){
 	var visited = new Map();
 	var to_visit = [];
 
 	to_visit.push(path);
-	
-	return process_url(domain,selector,to_visit,visited);
+	var bar = new ProgressBar('spider -> :bar :percent :total :elapsed', {
+		complete: '=',
+		incomplete: ' ',
+		width: 100,
+		total: 1
+	});
+
+	return process_url(domain,selector,to_visit,visited, bar);
 };
 
 
-function process_url(domain, selector, to_visit, visited){
+function process_url(domain, selector, to_visit, visited, bar){
 	var path=to_visit.shift();
 	return request(domain+path)
 		.then(function(body){
@@ -29,9 +36,14 @@ function process_url(domain, selector, to_visit, visited){
 					to_visit.push(new_path);
 				}
 			});
-			if(to_visit.length==0)
-				return visited;
 
-			return process_url(domain, selector, to_visit, visited);
+			if(to_visit.length==0){
+				bar.tick(1);
+				return visited;
+			}
+
+			bar.total=to_visit.length + visited.count();
+			bar.tick();
+			return process_url(domain, selector, to_visit, visited, bar);
 		});	
 }
