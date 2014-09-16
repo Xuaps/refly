ad47213459c5277f3e8bd7af51925200640c6b04
Alvaro Verdión <averdion@gmail.com>
2014-09-16 13:35:55 +0200

    deleted comments

--- public/js/WebClient/TreeView.js
+++ public/js/WebClient/TreeView.js
@@ -29,7 +29,6 @@ var TreeView = {
 		container.html('');
 		jade.render(container[0], 'tree-view', { collection: collection });
 		$.each(collection, function(key,item){
-			//container.append('<li><img src="/img/type-' + item.type + '.png" title="' + item.type + '" class="ry-type-source" /><a id="A' + jqSelector(item.uri) + '" href="' + ((item.uri[0] == '/') ? item.uri : '/' + item.uri) +'" class="treeviewitem">' + ((item.reference != '') ? item.reference : '(Titulo Vacio)') + '</a>' + ((item.len > 0) ? ' ('+ item.len +')' : '') + '<ul id="UL' + jqSelector(item.uri) + '"></ul></li>');
 			$('#A'+jqSelector(item.uri)).click(function(e){
 							e.preventDefault();
 							var url = RemoveBaseUrl(e.currentTarget.href);
@@ -47,9 +46,6 @@ var TreeView = {
 								reference.refresh('content');
 								reference.get('content', function(content) {
 									MarkdownViewer.show(content);
-								});
-								item.get_children(function(){
-									TreeView.toggle($('#UL' + jqSelector(item.uri)),item.children);
 								});
 
 							}
