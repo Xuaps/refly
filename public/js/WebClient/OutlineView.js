bcb55adfcf0a25d8f1831b41841631bea0547379
Alvaro Verdión <averdion@gmail.com>
2014-09-17 13:32:36 +0200

    outline V1

--- public/js/WebClient/OutlineView.js
+++ public/js/WebClient/OutlineView.js
@@ -7,6 +7,18 @@ var OutlineView = {
     show: function(reference) {
         OutlineView.reset();
         var symbols = {};
+		reference.get('parent', function(parent) {
+			parent.get_children(function(children){
+				$.each(children, function(key,item){
+		            if (!symbols[item.type]) {
+		                symbols[item.type] = [];
+		            }
+					symbols[item.type].push(item);
+				});
+				jade.render($('#outline-view')[0],'outline-view',{ symbols: symbols, current_uri: reference.uri });
+				$('#outline-view > div').accordion();
+			});
+		});
         //reference.get('root', function(root) {
             /*root.get('objects').each(function(object) {
                 if (!symbols[object.type]) {
