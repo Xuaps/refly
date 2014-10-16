39a1e06a9c7452c51b24022f87c871fa0b70900e
David Vilchez <dvilchez@xuaps.com>
2014-10-16 11:24:57 +0200

    clean treeview test

--- __tests__/TreeViewSpec.js
+++ __tests__/TreeViewSpec.js
@@ -10,10 +10,6 @@ var Link = require('react-router').Link;
 var storeMock = require('../public/js/store.js');
 
 describe('TreeView Component', function(){
-    beforeEach(function(){
-    
-    });
-
     describe('Initial State', function(){
         it('should have all docsets', function(){
             var treeview = render_treeview();
@@ -22,67 +18,67 @@ describe('TreeView Component', function(){
         });
     });
 
-    describe('Click in one docset', function() {
+    describe('TreeNode Component', function(){
+        describe('Click in one docset', function() {
+            it('should load all docset types', function() {
+                var treeview = render_treeview();
+                var treenode = emulate_click_docset(treeview,0)[0]; 
 
-        beforeEach(function(){
+                expect(treenode.props.parents.length).toEqual(1);
+                expect(treenode.state.data.length).toEqual(3);     
+            });
 
+            it('should not load all docset types of those have already been loaded', function(){
+                var treeview = render_treeview();
+                //workaround
+                var expected = storeMock.get.mock.calls.length+1;
+                var treenode = emulate_click_docset(treeview,0,2)[0]; 
+                
+                expect(storeMock.get.mock.calls.length).toEqual(expected);
+            });
+
+            it ('should change the children state hide/show', function(){
+                var treeview = render_treeview();
+                var res = emulate_click_docset(treeview,0); 
+                var treenode = res[0];
+                var link = res[1];
+
+                expect(treenode.state.show).toBe(true);
+                TestUtils.Simulate.click(link);
+                expect(treenode.state.show).toBe(false);
+            }); 
         });
 
-        it('should load all docset types', function() {
-            var treeview = render_treeview();
-            var treenode = emulate_click_docset(treeview,0)[0]; 
+        describe('Click in one type', function() {
+            it('should load all refrence by type and docset', function () {
+                var treeview = render_treeview();
+                var treenode_docset = emulate_click_docset(treeview,0)[0];
+                var treenode_type = emulate_click_docset(treenode_docset, 1)[0];
 
-            expect(treenode.props.parents.length).toEqual(1);
-            expect(treenode.state.data.length).toEqual(3);     
+                expect(treenode_type.props.parents.length).toEqual(2);
+                expect(treenode_type.state.data.length).toEqual(1);     
+            });
+
+            xit('should not load all refrences types those have already been loaded', function(){
+                //redundant
+            });
+
+            xit ('should change the children state hide/show', function(){
+                //redundant
+            }); 
         });
 
-        it('should not load all docset types of those have already been loaded', function(){
-            var treeview = render_treeview();
-            //workaround
-            var expected = storeMock.get.mock.calls.length+1;
-            var treenode = emulate_click_docset(treeview,0,2)[0]; 
-            
-            expect(storeMock.get.mock.calls.length).toEqual(expected);
-        });
-
-        it ('should change the children state hide/show', function(){
-            var treeview = render_treeview();
-            var res = emulate_click_docset(treeview,0); 
-            var treenode = res[0];
-            var link = res[1];
-
-            expect(treenode.state.show).toBe(true);
-            TestUtils.Simulate.click(link);
-            expect(treenode.state.show).toBe(false);
-        }); 
-    });
-
-    describe('Click in one type', function() {
-        it('should load all refrence by type and docset', function () {
-            var treeview = render_treeview();
-            var treenode_docset = emulate_click_docset(treeview,0)[0];
-            var treenode_type = emulate_click_docset(treenode_docset, 1)[0];
-
-            expect(treenode_type.props.parents.length).toEqual(2);
-            expect(treenode_type.state.data.length).toEqual(1);     
-        });
-    });
-
-   //click twice in docset or type dont reload data
-   //click in reference not load data
-   //click in docset hide and show types
-   //click in types hide and show references 
-
-    describe('Click in one reference', function() {
-        it('should nav to this reference', function() {
-            var treeview = render_treeview();
-            var treenode_docset = emulate_click_docset(treeview,0)[0];
-            var treenode_type = emulate_click_docset(treenode_docset,1)[0];
-            var treenode_ref = emulate_click_docset(treenode_type, 1)[0];
-            var link = TestUtils.findRenderedComponentWithType(treenode_ref, <Link/>);
-            
-            expect(treenode_ref.props.url).toBeDefined();
-            expect(link).toBeDefined();
+        describe('Click in one reference', function() {
+            it('should nav to this reference', function() {
+                var treeview = render_treeview();
+                var treenode_docset = emulate_click_docset(treeview,0)[0];
+                var treenode_type = emulate_click_docset(treenode_docset,1)[0];
+                var treenode_ref = emulate_click_docset(treenode_type, 1)[0];
+                var link = TestUtils.findRenderedComponentWithType(treenode_ref, <Link/>);
+                
+                expect(treenode_ref.props.url).toBeDefined();
+                expect(link).toBeDefined();
+            });
         });
     });
 
