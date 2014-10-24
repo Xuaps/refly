ba92818af0a75246335f5bb53cd030ca08b18291
Eru <eanovoa@gmail.com>
2014-10-24 15:16:34 +0200

    Add link to image logos in treeview

--- components/treenode.jsx
+++ components/treenode.jsx
@@ -21,14 +21,13 @@ var TreeNode = React.createClass({
         var item = this.props;
         var link;
         if(item.uri){
-           link = <Link to="result" params={{docset:item.docset, splat: item.uri}}>{item.name}</Link>;
+           link = <Link to="result" params={{docset:item.docset, splat: item.uri}}><img src={'/img/type-' + item.type + '.png'} title={item.type} className="ry-type-source"/>{item.name}</Link>;
         }else{
-           link = <a onClick={this.show}>{item.name}</a>;
+           link = <a onClick={this.show}><img src={'/img/type-' + item.type + '.png'} title={item.type} className="ry-type-source"/>{item.name}</a>;
         }            
         return (
             <li className={this.state.show?this.CHECKED_CLASS:''}>
                {item.type==='docset'?<img src={'/img/languages/' + item.name + '-logo.png'} title={item.name} className="ry-language-source"/>:''}
-               <img src={'/img/type-' + item.type + '.png'} title={item.type} className="ry-type-source"/>
                {link}
                <span className='cursive'>{item.len>0?'('+item.len+')':''}</span>
                <ul className={this.state.show?this.SHOW_CLASS:this.HIDE_CLASS}>
