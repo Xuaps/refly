4afc3aa7d506db86562d0da92daa4c6b31af0b39
David Vilchez <dvilchez@xuaps.com>
2014-11-13 12:12:21 +0100

    removed addRefsRange behaviour and its tests

--- app/docsets.js
+++ app/docsets.js
@@ -40,23 +40,4 @@ Docsets.prototype.execute = function() {
         });
 };
 
-Docsets.prototype.addRefsRange = function(refs) {
-    var query='';
-    refs.forEach(function(ref){
-            var where=ref.parent?
-                util.format("WHERE reference=$slash$%s$slash$ and type=$slash$%s$slash$ and docset=$slash$%s$slash$ and parent_id=(SELECT id FROM refs where uri=$slash$%s$slash$)",
-                     ref.reference, ref.type,ref.docset,ref.parent)
-                : util.format("WHERE reference=$slash$%s$slash$ and type=$slash$%s$slash$ and docset=$slash$%s$slash$ and parent_id is null",
-                     ref.reference, ref.type,ref.docset)
-            query+=util.format("\n\
-                    UPDATE refs SET content=$slash$%s$slash$, uri=$slash$%s$slash$ "+where+";\
-                    INSERT INTO refs ( reference, type, docset, content, uri, parent_id)\
-                    SELECT $slash$%s$slash$, $slash$%s$slash$, $slash$%s$slash$, $slash$%s$slash$, $slash$%s$slash$,(SELECT id FROM refs WHERE uri=$slash$%s$slash$)\
-                    WHERE NOT EXISTS (SELECT 1 FROM refs "+where+");"
-                    ,ref.content, ref.uri,ref.reference, ref.type,ref.docset,ref.content, ref.uri, ref.parent);
-        });
-    this._query = db.raw(query);
-    return this;
-};
-
 module.exports = Docsets; 
