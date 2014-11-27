a27e9ce654675cf08ed928dd07f3e81c85591b8e
Alvaro Verdión <averdion@gmail.com>
2014-11-27 13:46:18 +0100

    added field default uri to the global init

--- db/init.sql
+++ db/init.sql
@@ -34,6 +34,7 @@ create sequence Did_seq start 1;
 CREATE TABLE docsets (
     id INTEGER PRIMARY KEY default nextval('Did_seq'),
     docset TEXT,
+    defaulturi TEXT,
     pub_date TIMESTAMP default CURRENT_DATE,
     update_date TIMESTAMP,
     state TEXT
