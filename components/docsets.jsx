2b5dc742474523682519f45b70c6324eb3bc33b9
Alvaro Verdión <averdion@gmail.com>
2014-12-31 17:31:47 +0100

    link disabled in docset

--- components/docsets.jsx
+++ components/docsets.jsx
@@ -24,7 +24,7 @@ module.exports = React.createClass({
 			}else{
 				sticker = '';
 			}
-			if(item.date!=null && new Date(item.date)<new Date()){
+			if(item.date!=null && new Date(item.date)<new Date() && item.state=='active'){
 				var itemdate = new Date(item.date);
 				var infodate = <div className="docset-date ok-state">{String(itemdate.getDate()+100).substr(1) + '-' + String(itemdate.getMonth()+100).substr(1) + '-' + itemdate.getFullYear()}</div>
 			}else{
