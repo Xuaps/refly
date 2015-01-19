56711f52a1946179c833c243b00f131a3e689b36
Eru <eanovoa@gmail.com>
2015-01-19 09:21:53 +0100

    Fix scroll effect in landing when resize window

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,14 +1,22 @@
+
+var valor = 0;
+
+function scrollPosition(destino) {
+    valor = destino;
+    $('.docsets-container').scrollLeft(valor);
+}
+
 $(function() {
-    var thumbsW = $('.docsets-thumbs').innerWidth();
+    var thumbsW = 0;
     var containerW = 816;
-    var valor = 0;
     var offset = 0;
 
     $('.docsets-container').mousemove(function(e)
     {
+        thumbsW = $('.docsets-thumbs').innerWidth();
         containerW = $('.docsets-container').innerWidth();
         offset = $('.docsets-container').offset();
-        valor = ((thumbsW - containerW)*(((e.pageX-offset.left) / containerW).toFixed(3))).toFixed(1);
-        $('.docsets-container').scrollLeft(valor);
+        destino = ((thumbsW - containerW)*(((e.pageX-offset.left) / containerW).toFixed(3))).toFixed(1);
+        setInterval(scrollPosition(destino), 500);
     });
 });
