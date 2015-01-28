90cf8fa8b318a2e2533985a9e69c2f5975a267f3
David Vilchez <dvilchez@xuaps.com>
2015-01-28 08:00:48 +0100

    updated expressjs to 4, etags works

--- app.js
+++ app.js
@@ -4,31 +4,22 @@ var express = require('express')
   , api = require('./app/api.js')
   , http = require('http')
   , path = require('path')
+  , morgan = require('morgan')
+  , errorhandler = require('errorhandler')
   , config = require('config')
   , hal = require('express-hal');
 
 var app = express();
 
-app.configure(function(){
-  app.set('port', config.serverConfig.port);
-  app.set('ipaddr', config.serverConfig.ip);
-  app.set('view engine', 'jade');
-  app.use(favicon(path.join(__dirname,'public','img','favicon.ico')));
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(hal.middleware);
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use(function(req, res){
-        res.set('Content-Type', 'text/html');
-        res.sendfile('public/index.html');
-    });   
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
+app.set('port', config.serverConfig.port);
+app.set('ipaddr', config.serverConfig.ip);
+app.set('view engine', 'jade');
+app.use(favicon(path.join(__dirname,'public','img','favicon.ico')));
+app.use(morgan('dev'));
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
+//app.use(express.methodOverride());
+app.use(hal.middleware);
 
 app.get('/api', function(req, res){
     res.set('Content-Type', 'application/hal+json');
@@ -88,6 +79,17 @@ app.get('/api/types?', function(req, res){
             res.hal(hal);
         });
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res){
+    res.set('Content-Type', 'text/html');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});   
+
+var env = process.env.NODE_ENV || 'development';
+if ('development' == env) {
+  app.use(errorhandler());
+}
 
 http.createServer(app).listen(app.get('port'), app.get('ipaddr'), function(){
   console.log("Express server listening on port " + app.get('port'));
