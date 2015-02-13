7db1883b4e89953e38f365ac3ab656f8b5851d7a
David Vilchez <dvilchez@xuaps.com>
2015-02-13 13:14:02 +0100

    Patch docset name in searh_result link
    We need a new field to set the internal name of a docset. The name and
    internal identifier can be different.
    
    For example:
    dotnet - .net

--- components/search_result_row.jsx
+++ components/search_result_row.jsx
@@ -6,7 +6,7 @@ var Link = Router.Link;
 module.exports = React.createClass({
     render: function(){
         return (
-                <Link to="result" key={'RRL' + this.props.uri} className={"resultlistitem type-icon type-"+ this.props.type} params={{'docset': this.props.docset, splat: this.props.uri}}>
+                <Link to="result" key={'RRL' + this.props.uri} className={"resultlistitem type-icon type-"+ this.props.type} params={{'docset': this.props.docset.toLowerCase(), splat: this.props.uri}}>
                     <span className={"docset-icon docsets-" + this.props.docset}></span>
                     {this.props.reference}
                 </Link>
