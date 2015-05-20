7ef56ddee7225189ef3615c37a8c47baa4962eb6
Alvaro Verdión <averdion@gmail.com>
2015-05-20 10:21:40 +0200

    changed styles in welcome

--- react/welcome/welcome.jsx
+++ react/welcome/welcome.jsx
@@ -11,10 +11,11 @@ module.exports = React.createClass({
         	    </div></div>
         	    <div>
                     <h2>Howto</h2>
-                    <p className="paragraph">Browse through the documentation via treeview or via the search. All the documentation is hierarchically organized and categorized for a successful experience.</p>
-                    <p className="paragraph">By default, a group of docsets is displayed, however there are more, enable and disable them in <a href="/settings">settings</a> according to your technologic stack. Our labour:keeping documentations up-to-date; Yours:Enjoy the experience of solving all your doubts in one centered place.</p>
-                    <p className="paragraph"><span className='imageexample'><img src="/img/welcome1.png" /></span> <span className='imageexample'><img src="/img/welcome2.png" /></span> <span className='imageexample'><img src="/img/welcome3.png" /></span></p>
-                    <p className="paragraph"><a href="/signup">Sign Up</a> and improve the experience using refly.co.</p>
+                    <p className="paragraph">Browse through the documentation via treeview or via the search. All the documentation is hierarchically organized and categorized for a successful experience. By default, a group of docsets is displayed, however there are more, enable and disable them in <a href="/settings">settings</a> according to your technologic stack.</p>
+                    <blockquote>
+                    <div  className="jumbotron">Our labour: keeping documentations up-to-date; Yours: Enjoy the experience of solving all your doubts in one centered place.</div>
+                    </blockquote>
+                    <p className="center"><span className='imageexample'><img src="/img/welcome1.png" /></span> <span className='imageexample'><img src="/img/welcome2.png" /></span> <span className='imageexample'><img src="/img/welcome3.png" /></span></p>
                     <p className="center"><input type="button" className="btn btn-primary" value="Register now" /></p>
                 </div>
                 <div>
