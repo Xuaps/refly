a1d536ee4ec98f52247f42822a82ca98493b5d90
David Vilchez <dvilchez@xuaps.com>
2015-06-03 09:45:34 +0200

    removed usermanager and clientmanager specs

--- test/ClientManagerSpec.js
+++
@@ -1,9 +0,0 @@
-var session = require('../routes/client-manager.js');
-
-describe('client-manager', function(){
-    it('should create one new client id if it doesnt exist', function(){
-        var req = {session:{}};
-        session(req);
-        expect(req.session.client_id).toBeDefined(); 
-    });
-});
