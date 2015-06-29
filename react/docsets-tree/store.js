60a7ecdeeddbe192fbc45c23fe94e10ce7f441dc
Alvaro Verdión <averdion@gmail.com>
2015-06-29 14:18:40 +0200

    improvements in treeview

--- react/docsets-tree/store.js
+++ react/docsets-tree/store.js
@@ -16,24 +16,20 @@ module.exports = Reflux.createStore({
 
     onGetActiveDocsets: function(){
         this.docsets = JSON.parse(JSON.stringify(settings.getWorkingDocsets()));
-        this.flatten_elements = this.flatten_elements.concat(this.docsets);
         this.trigger(this.docsets);
     },
 
     onGetTypes: function(docset){
         var active_docset = this.docsets
             .filter(function(doc){ return doc.name === docset; })[0];
-        this.flatten_elements.forEach(function(el){ el.marked = (el.name === docset) });
         if(active_docset.types){
             this.trigger(this.docsets);
             return;
         }
-        data.getTypes(docset).then(function(response){ 
+        setTimeout(function(){data.getTypes(docset).then(function(response){ 
             active_docset.types = response['_embedded']['rl:types']; 
-            this.flatten_elements = this.flatten_elements.concat(active_docset.types.map(
-                    function(typ){typ.docset = docset; return typ;}));
             this.trigger(this.docsets);
-        }.bind(this)).done();
+        }.bind(this)).done()}.bind(this));
     },
 
     onSearchReferences: function(docset, type_name, page){
@@ -41,20 +37,18 @@ module.exports = Reflux.createStore({
             .filter(function(doc){ return doc.name === docset;})[0]
             .types.filter(function(type){ return type.name === type_name; })[0];
         if(!page){
-            this.flatten_elements.forEach(function(el){ el.marked = (el.name === type_name && el.docset ===docset) });
             if(node.references){
                 this.trigger(this.docsets);
                 return;
             }
         }
         page = page || 1;
-        data.getReferences(docset, type_name, page, PAGE_SIZE).then(function (response){
+        setTimeout(function(){data.getReferences(docset, type_name, page, PAGE_SIZE).then(function (response){
             node.references = node.references || [];
             node.references = node.references.concat(response['_embedded']['rl:references']);
-            this.flatten_elements = this.flatten_elements.concat(node.references);
             this.trigger(this.docsets);    
 
             return response['_links'].next?this.onSearchReferences(docset, type_name, page+1):undefined;
-        }.bind(this)).done();
+        }.bind(this)).done()}.bind(this),0);
     },
 });
