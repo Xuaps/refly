1ad7e7b91336b7aeda5b3b1ccd87f47525c49b0c
Alvaro Verdión <averdion@gmail.com>
2015-06-30 13:34:07 +0200

    moved settings buttong close to searchbox Please enter the commit message for your changes. Lines starting

---
+++ react/settings/settings-button.jsx
@@ -0,0 +1,25 @@
+var React = require('react');
+var URI = require ('URIjs');
+var Router = require('react-router');
+var Ladda = require('ladda');
+
+module.exports = React.createClass({
+    mixins: [ Router.State, Router.Navigation ],
+    getInitialState: function(){
+        return {last_search: this.getQuery().ref || ''};
+    },
+
+    render: function(){
+        return(<span>
+               <a id="btnsettings" className="ladda-button btn-settings" data-spinner-color="#222" data-style="zoom-in" data-size="xs" onClick={function(e){e.preventDefault();this.navigation("settings");}.bind(this)}><span className="ladda-label"><span className="glyphicon glyphicon-filter" aria-hidden="true"></span></span></a>
+               </span>);
+    },
+
+    navigation: function(uri){
+        var l = Ladda.create( document.querySelector( '#btnsettings' ) );
+        l.start();
+        this.transitionTo(uri);
+        setTimeout(function(){l.stop()},2000);
+    },
+
+});
