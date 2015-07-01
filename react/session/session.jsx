09e8f8c5507f5de411e22208c5a930ea99520680
Alvaro Verdión <averdion@gmail.com>
2015-07-01 11:41:08 +0200

    corrected texts and dispositions

--- react/session/session.jsx
+++ react/session/session.jsx
@@ -1,17 +1,12 @@
 var React = require('react');
 var SessionForm = require('./session-form.jsx');
 var SubscriptionData = require('../payment/subscription-data.jsx');
-var Legal = require('../components/legal.jsx');
 
 module.exports = React.createClass({
       render: function (){
-          return <div><SessionForm query={this.props.query}>
+          return <SessionForm query={this.props.query}>
                     <SubscriptionData/>
                 </SessionForm>
-                <blockquote><h1>In case you want to know more about our privacy policy look up below.</h1></blockquote>
-                <Legal ref="legalcomponent"></Legal>
-                </div>
-
 
        }
 });
