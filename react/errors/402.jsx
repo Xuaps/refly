f5650096425e0efe77aa5c1b4cb9fb795b070b33
Alvaro Verdión <averdion@gmail.com>
2015-07-03 10:08:31 +0200

    changed text of the seggestion

--- react/errors/402.jsx
+++ react/errors/402.jsx
@@ -9,7 +9,7 @@ module.exports = React.createClass({
         return <div>
                     <div className="row text-center">
                         <CountdownClock seconds={10} color="#000" alpha={0.9} size={150} onComplete={this.props.onComplete} />
-                        <h2 className='col-xs-12 text-center'>Please consider upgrade to PRO to avoid this message</h2>
+                        <h2 className='col-xs-12 text-center'>Please considering upgrading to PRO to avoid this message</h2>
                     </div>
                     <div className="row">
                         <div className="h1 col-xs-12"></div>
