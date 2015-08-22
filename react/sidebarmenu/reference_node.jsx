91b3f4008bb11b554e6560b1b13d6ae25fa6570a
Alvaro Verdión <averdion@gmail.com>
2015-08-22 12:16:06 +0200

    added background to selected item

--- react/sidebarmenu/reference_node.jsx
+++ react/sidebarmenu/reference_node.jsx
@@ -1,7 +1,18 @@
 var React = require('react');
-
+var URI = require('URIjs');
 module.exports = React.createClass({
     render: function(){
-        return <a className={"list-group-item type-icon type-" + this.props.reference.type} onClick={this.props.onClickReference} key={this.props.nodeKey} href={this.props.reference.uri}><span className={"docset-icon docsets-" + this.props.selected_docset.name}></span> {this.props.reference.name}</a>;
+    	var className = '';
+        if (this.isSelected()){
+            className += ' selected';
+        }
+        return <a className={"list-group-item type-icon type-" + this.props.reference.type + className} onClick={this.props.onClickReference} key={this.props.nodeKey} href={this.props.reference.uri}><span className={"docset-icon docsets-" + this.props.selected_docset.name}></span> {this.props.reference.name}</a>;
+    },
+
+    isSelected: function(){
+        var baseUri=new URI(window.document.baseURI);
+        var nodeuri = baseUri.protocol() + '://' + baseUri.host() + this.props.reference.uri;
+        return window.document.baseURI == nodeuri;
     }
+
 });
