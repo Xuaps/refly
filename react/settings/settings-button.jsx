f0b830e1c0eb7f49e87140dc8089456730e04d6b
Alvaro Verdión <averdion@gmail.com>
2015-12-17 19:32:42 +0100

    added tooltips to setting buttom

--- react/settings/settings-button.jsx
+++ react/settings/settings-button.jsx
@@ -11,10 +11,14 @@ module.exports = React.createClass({
 
     render: function(){
         return(<span>
-               <button id="btnsettings" className="ladda-button btn-settings" data-spinner-color="#222" data-style="zoom-in" data-size="xs" onClick={function(e){e.preventDefault();this.navigation("settings");}.bind(this)}><span className="ladda-label"><em className="icon-list"></em></span></button>
+               <button id="btnsettings" data-toggle="tooltip" data-placement="left" className="ladda-button btn-settings" data-spinner-color="#222" data-style="zoom-in" data-size="xs" onClick={function(e){e.preventDefault();this.navigation("settings");}.bind(this)}><span className="ladda-label"><em className="icon-list"></em></span></button>
                </span>);
     },
 
+    componentDidMount: function() {
+        this.showTooltip();
+    },
+    
     navigation: function(uri){
         var l = Ladda.create( document.querySelector( '#btnsettings' ) );
         l.start();
@@ -22,5 +26,18 @@ module.exports = React.createClass({
         setTimeout(function(){l.stop()},2000);
         $('.row-offcanvas').toggleClass('active');
     },
+
+    showTooltip: function(){
+      var options = {
+                    placement: 'left',
+                    delay: { "show": 0, "hide": 10 },
+                    title: 'View All Docsets',
+                    template : '<div class="tooltip" role="tooltip"><div class="tooltip-arrow"></div><div style="border-radius: 5px;" class="tooltip-inner"></div></div>',
+                };
+      $('#btnsettings').tooltip(options);
+      $('#btnsettings').tooltip('show');
+      $('.tooltip-inner').html('View All Docsets<br /> Configure your stack')
+      setTimeout(function(){$('#btnsettings').tooltip('hide');},5000);
+    }
 
 });
