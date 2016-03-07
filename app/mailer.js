6eb46b24e41e74e7138fc1aabb9aaa01dd2aa89c
Alvaro Verdión <averdion@gmail.com>
2016-03-07 13:26:02 +0100

    removed traces

--- app/mailer.js
+++ app/mailer.js
@@ -18,13 +18,13 @@ module.exports.sendMailTemplated = function(email, template_config){
         }
     });
     return sendgrid.send(SGEmail, function(err, json) {
+      console.log(json);
       if (err) {
         console.log('A sendgrid error occurred: ' + err);
         return false;
       }else{
         return true;
       }
-      // console.log(json);
     });
 };
 
