5009530eb79a8143da72ddfb7e3d1ae6b84a483b
Alvaro Verdión <averdion@gmail.com>
2016-03-08 18:56:31 +0100

    returning to a previous versión.

--- app/users.js
+++ app/users.js
@@ -11,7 +11,7 @@ Users.prototype.find = function(values){
         .then(function(users){
             if(users.length !== 1)
                 return [];
-            return users[0];
+            return users;
         });
 };
 
